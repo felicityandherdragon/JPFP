7cc060383cf9a4e564d88a70f5e06fa6e30fecc0
Stanley <lims95@yahoo.com>
2021-04-20 21:07:04 -0400

    changed image things

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React from 'react';
+import img from '../../public/images/S4_Artboard Copy 6.jpg';
 
 const Home = () => {
-  return (
-    <div id='display'>
-      <div id='home-slider'>
-        <h2>Welcome home!</h2>
-        <p>Best resolution: <b>1200 - 1400px</b> <br /> AKA "I did not manage to fix styling issues when screens get bigger or smaller"</p>
-        <img src='../../public/images/S4_Artboard Copy 6.jpg' />
-        <p>I added the homepage but then didn't know what extra stuff i can add here.
-        So here is one single random image for the homepage.</p>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div id='display'>
+			<div id='home-slider'>
+				<h2>Welcome home!</h2>
+				<p>
+					Best resolution: <b>1200 - 1400px</b> <br /> AKA "I did not manage to
+					fix styling issues when screens get bigger or smaller"
+				</p>
+				<img src={img} />
+				<p>
+					I added the homepage but then didn't know what extra stuff i can add
+					here. So here is one single random image for the homepage.
+				</p>
+			</div>
+		</div>
+	);
+};
 
-export default Home
-
-
-
+export default Home;
